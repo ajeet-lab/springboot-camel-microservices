57231ae3e44b7adb8be261dbaea256320b405ddf
AJEET KUSHWAHA <51816732+ajeet-lab@users.noreply.github.com>
2024-04-11 14:46:01 +0530

    update code

--- SpliteAndAggregation/src/main/java/com/aggregate/MySpringBootApplication.java
+++ SpliteAndAggregation/src/main/java/com/aggregate/MySpringBootApplication.java
@@ -7,12 +7,7 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 @ImportResource({"classpath:spring/camel-context.xml"})
 public class MySpringBootApplication {
-
-    /**
-     * A main method to start this application.
-     */
     public static void main(String[] args) {
         SpringApplication.run(MySpringBootApplication.class, args);
     }
-
 }
